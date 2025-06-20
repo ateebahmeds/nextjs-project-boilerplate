0616f97aea212ea42e08b9a5cf39d232bdffae54
ateebahmeds <110182945+ateebahmeds@users.noreply.github.com>
2025-06-20 06:15:34 +0500

    Add bookstore-management-shared/Models/Author.cs 1750382132021

---
+++ bookstore-management-shared/Models/Author.cs
@@ -0,0 +1,9 @@
+namespace bookstore_management_shared.Models
+{
+    public class Author
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public ICollection<Book>? Books { get; set; }
+    }
+}
